efb43b91a2ceadcf7ba014818cdbb0dd9879630a
Antoine Stollsteiner <astollsteiner@fluksaqua.com>
2019-07-17 12:00:44 +0200

    add addition example

--- pages/bitonic.js
+++ pages/bitonic.js
@@ -9,129 +9,9 @@ let bitonicSortProgram1;
 let bitonicSortProgram2;
 let bitonicSortProgram2UniformLocation;
 
-const main = async () => {
-  // Selector setup
-  selectBox = document.getElementById('selectBox');
-  const maxNumElementsIndex = Math.log2(MAX_THREAD_NUM * MAX_GROUP_NUM) - 9;
-  for (let i = 0; i < maxNumElementsIndex; i++) {
-    const option = document.createElement('option');
-    option.text = '' + getLength(i);
-    selectBox.add(option);
-  }
-  selectBox.selectedIndex = 7;
-  selectBox.addEventListener('change', () => {
-    logElement.innerText = '';
-    selectBox.disabled = true;
-    requestAnimationFrame(() => compute());
-  });
-  selectBox.disabled = true;
-
-  // Div setup
-  logElement = document.getElementById('log');
-
-  // Canvas setup
-  const canvas = document.createElement(('canvas'));
-
-  // Create WebGL2ComputeRenderingContext
-  context = canvas.getContext('webgl2-compute');
-  if (!context) {
-    document.body.className = 'error';
-    return;
-  }
-
-  const initializeResult = initializeComputeProgram();
-  if (!initializeResult) {
-    return;
-  }
-
-  compute();
-};
-
-const compute = async () => {
-  const length = getLength(selectBox.selectedIndex);
-  console.log(`sort test: ${length}`);
-  const arr = new Float32Array(length);
-  resetData(arr, length);
-
-  await computeCPU(arr.slice(0));
-  await computeGPU(arr.slice(0));
-
-  selectBox.disabled = false;
-  console.log(`---`);
-};
-
-const computeCPU = async (arr) => {
-  const now = performance.now();
-  arr.sort(
-    (a, b) => {
-      return a - b;
-    }
-  );
-  log(`CPU sort time: ${Math.round(performance.now() - now)} ms`);
-  console.log(`CPU sort result validation: ${validateSorted(arr) ? 'success' : 'failure'}`);
-
-  // console.log(arr);
-};
-
-const computeGPU = async (arr) => {
-  const now = performance.now();
-
-  const length = arr.length;
-
-  const threadgroupsPerGrid = Math.max(1, length / MAX_THREAD_NUM);
-
-  // create ShaderStorageBuffer
-  const ssbo = context.createBuffer();
-  context.bindBuffer(context.SHADER_STORAGE_BUFFER, ssbo);
-  context.bufferData(context.SHADER_STORAGE_BUFFER, arr, context.DYNAMIC_COPY);
-  context.bindBufferBase(context.SHADER_STORAGE_BUFFER, 0, ssbo);
-
-  // execute ComputeShader
-  context.useProgram(bitonicSortProgram1);
-  context.dispatchCompute(threadgroupsPerGrid, 1, 1);
-  context.memoryBarrier(context.SHADER_STORAGE_BARRIER_BIT);
-
-  if (threadgroupsPerGrid > 1) {
-    for (let k = threadgroupsPerGrid; k <= length; k <<= 1) {
-      for (let j = k >> 1; j > 0; j >>= 1) {
-        // execute ComputeShader
-        context.useProgram(bitonicSortProgram2);
-        context.uniform4uiv(bitonicSortProgram2UniformLocation, new Uint32Array([k, j, 0, 0]));
-        context.dispatchCompute(threadgroupsPerGrid, 1, 1);
-        context.memoryBarrier(context.SHADER_STORAGE_BARRIER_BIT);
-      }
-    }
-  }
-
-  // get result
-  const result = new Float32Array(length);
-  context.getBufferSubData(context.SHADER_STORAGE_BUFFER, 0, result);
-  log(`GPU sort time: ${Math.round(performance.now() - now)} ms`);
-  console.log(`GPU sort result validation: ${validateSorted(result) ? 'success' : 'failure'}`);
-};
-
-const resetData = (arr, sortLength) => {
-  for (let i = 0; i < sortLength; i++) {
-    arr[i] = Math.random();
-  }
-};
-
-const validateSorted = (arr) => {
-  const length = arr.length;
-  for (let i = 0; i < length; i++) {
-    if (i !== length - 1 && arr[i] > arr[i + 1]) {
-      console.log('validation error:', i, arr[i], arr[i + 1]);
-      console.log(arr);
-      return false;
-    }
-  }
-  return true;
-};
-
-const initializeComputeProgram = () => {
-  // ComputeShader source
-  // language=GLSL
-  const computeShaderSource1 = `#version 310 es
+// ComputeShader source
+// language=GLSL
+const computeShaderSource1 = `#version 310 es
     layout (local_size_x = ${MAX_THREAD_NUM}, local_size_y = 1, local_size_z = 1) in;
     layout (std430, binding = 0) buffer SSBO {
       float data[];
@@ -174,14 +54,8 @@ const initializeComputeProgram = () => {
     }
     `;
 
-  // create WebGLProgram for ComputeShader
-  bitonicSortProgram1 = createComputeProgram(computeShaderSource1);
-  if (!bitonicSortProgram1) {
-    return false;
-  }
-
-  // language=GLSL
-  const computeShaderSource2 = `#version 310 es
+// language=GLSL
+const computeShaderSource2 = `#version 310 es
     layout (local_size_x = ${MAX_THREAD_NUM}, local_size_y = 1, local_size_z = 1) in;
     layout (std430, binding = 0) buffer SSBO {
       float data[];
@@ -215,44 +89,178 @@ const initializeComputeProgram = () => {
     }
     `;
 
-  // create WebGLProgram for ComputeShader
-  bitonicSortProgram2 = createComputeProgram(computeShaderSource2);
-  if (!bitonicSortProgram2) {
-    return false;
-  }
-  bitonicSortProgram2UniformLocation = context.getUniformLocation(bitonicSortProgram2, 'numElements');
+const main = async () => {
+    // Selector setup
+    setSelectBox()
+    run()
+};
 
-  return true;
+function setSelectBox(){
+    selectBox = document.getElementById('selectBox');
+    const maxNumElementsIndex = Math.log2(MAX_THREAD_NUM * MAX_GROUP_NUM) - 9;
+    for (let i = 0; i < maxNumElementsIndex; i++) {
+        const option = document.createElement('option');
+        option.text = '' + getLength(i);
+        selectBox.add(option);
+    }
+    selectBox.selectedIndex = 7;
+    selectBox.addEventListener('change', () => {
+        logElement.innerText = '';
+        selectBox.disabled = true;
+        requestAnimationFrame(() => compute());
+    });
+    selectBox.disabled = true;
+
+    // Div setup
+    logElement = document.getElementById('log');
+}
+
+function run(){
+    console.log('run')
+    getGLContext()
+    linkPrograms()
+    doCompute()
+}
+
+function getGLContext() {
+    console.log('getting context')
+    const canvas = document.createElement(('canvas'));
+    canvas.addEventListener("webglcontextlost", function(event) {
+        console.log('lost context')
+        event.preventDefault();
+        run()
+    }, false);
+    // Create WebGL2ComputeRenderingContext
+    context = canvas.getContext('webgl2-compute');
+}
+
+const doCompute = () => {
+    const length = getLength(selectBox.selectedIndex);
+    console.log(`sort test: ${length}`);
+    const arr = new Float32Array(length);
+    resetData(arr, length);
+
+    computeCPU(arr.slice(0));
+    computeGPU(arr.slice(0));
+
+    selectBox.disabled = false;
+    console.log(`---`);
+};
+
+const computeCPU = (arr) => {
+    const now = performance.now();
+    arr.sort(
+        (a, b) => {
+            return a - b;
+        }
+    );
+    log(`CPU sort time: ${Math.round(performance.now() - now)} ms`);
+    console.log(`CPU sort result validation: ${validateSorted(arr) ? 'success' : 'failure'}`);
+
+    // console.log(arr);
+};
+
+const computeGPU = (arr) => {
+    const now = performance.now();
+
+    const length = arr.length;
+
+    const threadgroupsPerGrid = Math.max(1, length / MAX_THREAD_NUM);
+
+    // create ShaderStorageBuffer
+    const ssbo = context.createBuffer();
+    context.bindBuffer(context.SHADER_STORAGE_BUFFER, ssbo);
+    context.bufferData(context.SHADER_STORAGE_BUFFER, arr, context.DYNAMIC_COPY);
+    context.bindBufferBase(context.SHADER_STORAGE_BUFFER, 0, ssbo);
+
+    // execute ComputeShader
+    context.useProgram(bitonicSortProgram1);
+    context.dispatchCompute(threadgroupsPerGrid, 1, 1);
+    context.memoryBarrier(context.SHADER_STORAGE_BARRIER_BIT);
+
+    if (threadgroupsPerGrid > 1) {
+        for (let k = threadgroupsPerGrid; k <= length; k <<= 1) {
+            for (let j = k >> 1; j > 0; j >>= 1) {
+                // execute ComputeShader
+                context.useProgram(bitonicSortProgram2);
+                context.uniform4uiv(bitonicSortProgram2UniformLocation, new Uint32Array([k, j, 0, 0]));
+                context.dispatchCompute(threadgroupsPerGrid, 1, 1);
+                context.memoryBarrier(context.SHADER_STORAGE_BARRIER_BIT);
+            }
+        }
+    }
+
+    // get result
+    const result = new Float32Array(length);
+    context.getBufferSubData(context.SHADER_STORAGE_BUFFER, 0, result);
+    log(`GPU sort time: ${Math.round(performance.now() - now)} ms`);
+    console.log(`GPU sort result validation: ${validateSorted(result) ? 'success' : 'failure'}`);
+};
+
+const resetData = (arr, sortLength) => {
+    for (let i = 0; i < sortLength; i++) {
+        arr[i] = Math.random();
+    }
+};
+
+const validateSorted = (arr) => {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        if (i !== length - 1 && arr[i] > arr[i + 1]) {
+            console.log('validation error:', i, arr[i], arr[i + 1]);
+            console.log(arr);
+            return false;
+        }
+    }
+    return true;
+};
+
+const linkPrograms = () => {
+
+    // create WebGLProgram for ComputeShader
+    bitonicSortProgram1 = createComputeProgram(computeShaderSource1);
+    if (!bitonicSortProgram1) {
+        return false;
+    }
+
+    // create WebGLProgram for ComputeShader
+    bitonicSortProgram2 = createComputeProgram(computeShaderSource2);
+    if (!bitonicSortProgram2) {
+        return false;
+    }
+    bitonicSortProgram2UniformLocation = context.getUniformLocation(bitonicSortProgram2, 'numElements');
+
+    return true;
 };
 
 const createComputeProgram = (source) => {
-  // create WebGLShader for ComputeShader
-  const computeShader = context.createShader(context.COMPUTE_SHADER);
-  context.shaderSource(computeShader, source);
-  context.compileShader(computeShader);
-  if (!context.getShaderParameter(computeShader, context.COMPILE_STATUS)) {
-    console.log(context.getShaderInfoLog(computeShader));
-    return null;
-  }
+    // create WebGLShader for ComputeShader
+    const computeShader = context.createShader(context.COMPUTE_SHADER);
+    context.shaderSource(computeShader, source);
+    context.compileShader(computeShader);
+    if (!context.getShaderParameter(computeShader, context.COMPILE_STATUS)) {
+        console.log(context.getShaderInfoLog(computeShader));
+        return null;
+    }
 
-  // create WebGLProgram for ComputeShader
-  const computeProgram = context.createProgram();
-  context.attachShader(computeProgram, computeShader);
-  context.linkProgram(computeProgram);
-  if (!context.getProgramParameter(computeProgram, context.LINK_STATUS)) {
-    console.log(context.getProgramInfoLog(computeProgram));
-    return null;
-  }
+    // create WebGLProgram for ComputeShader
+    const computeProgram = context.createProgram();
+    context.attachShader(computeProgram, computeShader);
+    context.linkProgram(computeProgram);
+    if (!context.getProgramParameter(computeProgram, context.LINK_STATUS)) {
+        console.log(context.getProgramInfoLog(computeProgram));
+        return null;
+    }
 
-  return computeProgram;
+    return computeProgram;
 };
 
 const getLength = (index) => {
-  return 1 << (index + 10);
+    return 1 << (index + 10);
 };
 
 const log = (str) => {
-  logElement.innerText += str + '\n';
+    logElement.innerText += str + '\n';
 };
 
 
